7c9dd3f7d258fc270797a4ea0522f19bee140877
Your Name <you@example.com>
2021-11-27 01:31:39 +0500

    - code refactoring

--- filters/filters.py
+++ filters/filters.py
@@ -4,13 +4,10 @@ def get_even_dict(raw_data: dict) -> dict:
 
 
 def get_dict_consisting_of_string(raw_data: dict) -> dict:
-    print(raw_data)
     raw_data = {str.lower(key): value for key, value in raw_data.items() if isinstance(key, str) and key.isalpha()}
     return raw_data
 
 
 def get_not_none_value_dict(raw_data: dict) -> dict:
-    print(raw_data)
     raw_data = {key: value for key, value in raw_data.items() if value}
-    print(raw_data)
     return raw_data
